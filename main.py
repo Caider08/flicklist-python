4fdc379c0ba8e573496fadeae75fa1e0d9a6b699
Caid Buchman <caibuch2010@gmail.com>
2017-01-26 21:07:15 -0600

    kdl;gjasd

--- main.py
+++ main.py
@@ -103,17 +103,28 @@ class AddMovie(webapp2.RequestHandler):
 
         # TODO 2
         # if the user typed nothing at all, redirect and yell at them
+        if new_movie == "":
+            blank_error = "You forgot to type something in Dummy!"
+            blank_error_escaped = cgi.escape(blank_error, quote=True)
+
+            self.redirect("/?error=" + blank_error_escaped)
+
 
 
         # TODO 3
         # if the user wants to add a terrible movie, redirect and yell at them
+        if new_movie in terrible_movies:
+            terrible_attempt = "You're movie sucks brah"
+            terrible_attempt_escaped = cgi.escape(terrible_attempt, quote=True)
 
+            self.redirect("/?error=" + terrible_attempt_escaped)
 
         # TODO 1
         # 'escape' the user's input so that if they typed HTML, it doesn't mess up our site
+        new_movie_escaped = cgi.escape(new_movie)
 
         # build response content
-        new_movie_element = "<strong>" + new_movie + "</strong>"
+        new_movie_element = "<strong>" + new_movie_escaped + "</strong>"
         sentence = new_movie_element + " has been added to your Watchlist!"
         content = page_header + "<p>" + sentence + "</p>" + page_footer
         self.response.write(content)
